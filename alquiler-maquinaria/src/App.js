5838974748088d97738265713a6ffda080fa02f8
Bryan Quintero <bryanquinterog@gmail.com>
2022-11-03 18:56:26 -0500

    *

--- alquiler-maquinaria/src/App.js
+++ alquiler-maquinaria/src/App.js
@@ -11,7 +11,6 @@ function App() {
     return (
         <Container>
             <div className="App">
-
                 <BrowserRouter>
                     <Navbar />
                     <Routes>
