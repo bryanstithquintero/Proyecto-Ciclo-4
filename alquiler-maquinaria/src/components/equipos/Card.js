f632cb3b803d3ec04920df3df50ef5e1c4e6c8e1
Bryan Quintero <bryanquinterog@gmail.com>
2022-11-15 17:54:53 -0500

    tarjetas metodo

--- alquiler-maquinaria/src/components/equipos/Card.js
+++ alquiler-maquinaria/src/components/equipos/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import image1 from '../../assets/image1.jpg'
+
 import './card.css'
 
-function Card() {
+function Card({ title, imageSource, descripcion }) {
     return (
 
         <div className="card text-center bg-light">
-            <img src={image1} alt='maquina descripcion'></img>
+            <img src={imageSource} alt='maquina descripcion'></img>
             <div className='card-body text-dark'>
-                <h4 className='card-title'> titulo maquina</h4>
-                <p className='card-text text-secondary'>descripcion general de la maquina</p>
+                <h4 className='card-title'> {title}</h4>
+                <p className='card-text text-secondary'>{descripcion}</p>
                 <button type='button' className='btn btn-outline-secondary rounded-0'>
                     agregar a reserva
                 </button>
