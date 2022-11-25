401a2fb53fa4b1125811bfcceb35c23106f3be0d
Bryan Quintero <bryanquinterog@gmail.com>
2022-11-24 20:11:43 -0500

    img espacio a la base de datos

--- proyecto_back/controller/maquinas.controller.js
+++ proyecto_back/controller/maquinas.controller.js
@@ -1,72 +1,74 @@
 const Maquina = require("../models/maquinas.model")
 
-let response ={msg:"",exito:false}
-exports.create = function(req,res){
-    let maquina = new Maquina ({
-        id_maquina:req.body.id_maquina,
+let response = { msg: "", exito: false }
+exports.create = function (req, res) {
+    let maquina = new Maquina({
+        id_maquina: req.body.id_maquina,
         nombre: req.body.nombre,
-        categoria:req.body.categoria,
-        marca:req.body.marca,
-        unidades:req.body.unidades,
-        novedad:req.body.novedad
-       
+        categoria: req.body.categoria,
+        marca: req.body.marca,
+        unidades: req.body.unidades,
+        novedad: req.body.novedad,
+        img: req.body.img
+
     })
-    maquina.save(function(err){
-        if(err){
+    maquina.save(function (err) {
+        if (err) {
             console.error(err),
-            response.exito = false,
-            response.msg = "Error al guardar la maquina"
+                response.exito = false,
+                response.msg = "Error al guardar la maquina"
             res.json(response)
             return;
-        } 
+        }
         response.exito = true,
-        response.msg = "La maquina se guardo correctamente"
+            response.msg = "La maquina se guardo correctamente"
         res.json(response)
     })
 }
-exports.find = function(req,res){
-    Maquina.find(function(err,maquinas){
+exports.find = function (req, res) {
+    Maquina.find(function (err, maquinas) {
         res.json(maquinas)
     })
 }
-exports.findOne = function(req,res){
-    Maquina.findOne({_id: req.params.id},function(err,maquina){
+exports.findOne = function (req, res) {
+    Maquina.findOne({ _id: req.params.id }, function (err, maquina) {
         res.json(maquina)
     })
 }
-exports.update = function(req,res){
+exports.update = function (req, res) {
     let maquina = {
-        id_maquina:req.body.id_maquina,
+        id_maquina: req.body.id_maquina,
         nombre: req.body.nombre,
-        categoria:req.body.categoria,
-        marca:req.body.marca,
-        unidades:req.body.unidades,
-        novedad:req.body.novedad
+        categoria: req.body.categoria,
+        marca: req.body.marca,
+        unidades: req.body.unidades,
+        novedad: req.body.novedad,
+        img: req.body.img
     }
-    Maquina.findByIdAndUpdate(req.params.id,{$set:maquina},function(err){
-        if(err){
+    Maquina.findByIdAndUpdate(req.params.id, { $set: maquina }, function (err) {
+        if (err) {
             console.error(err),
-            response.exito = false,
-            response.msg = "Error al modificar la maquina"
+                response.exito = false,
+                response.msg = "Error al modificar la maquina"
             res.json(response)
             return;
         }
         response.exito = true,
-        response.msg ="La maquina se modifico correctamente"
+            response.msg = "La maquina se modifico correctamente"
         res.json(response)
     })
 }
-exports.remove = function(req,res){
-    Maquina.findByIdAndRemove({_id:req.params.id},function(err){
-        if(err){
+exports.remove = function (req, res) {
+    Maquina.findByIdAndRemove({ _id: req.params.id }, function (err) {
+        if (err) {
             console.error(err),
-            response.exito = false,
-            response.msg = "Error al eliminar la maquina"
+                response.exito = false,
+                response.msg = "Error al eliminar la maquina"
             res.json(response)
             return;
         }
         response.exito = true,
-        response.msg = "La maquina fue eliminado correctamente"
+            response.msg = "La maquina fue eliminado correctamente"
         res.json(response)
     })
 }
--- proyecto_back/models/maquinas.model.js
+++ proyecto_back/models/maquinas.model.js
@@ -8,6 +8,7 @@ const MaquinasSchema = new Schema({
     marca: { type: String, require: true, max: 40 },
     unidades: { type: Number, require: true, max: 150 },
     novedad: { type: String, require: false, max: 15 },
+    img: { type: String, require: true, max: 150 },
 });
 
 module.exports = mongoose.model("maquinas", MaquinasSchema);
