6d9ba206eb306b92c19524cb35e9b21d13d4ee3d
Audrey Lopez <audrey@email.com>
2022-11-29 22:07:49 -0500

    depliegue 3

--- proyecto_back/config/database.js
+++ proyecto_back/config/database.js
@@ -4,13 +4,21 @@ const mongoose = require("mongoose");
 //const port ="27017";
 //const db ="proyecto4";
 
-exports.mongoConnect = () => {
+//exports.mongoConnect = () => {
+//const mongoStringConnection = `mongodb+srv://admin:admin@proyectociclo4.7zkmeab.mongodb.net/dbproyecto?retryWrites=true&w=majority`;
+//mongoose.connect(mongoStringConnection);
+// mongoose.Promise = global.Promise;
+//const dbConnection = mongoose.connection;
+//dbConnection.on("error",console.error.bind(console, "Mongodb connection error"));
+//};
+
+exports.mongoConnect = async () => {
   const mongoStringConnection = `mongodb+srv://admin:admin@proyectociclo4.7zkmeab.mongodb.net/dbproyecto?retryWrites=true&w=majority`;
-  mongoose.connect(mongoStringConnection);
-  mongoose.Promise = global.Promise;
-  const dbConnection = mongoose.connection;
-  dbConnection.on(
-    "error",
-    console.error.bind(console, "Mongodb connection error")
-  );
+  try {
+    await mongoose.connect(mongoStringConnection);
+    console.log("db conect succesful");
+  } catch (error) {
+    console.log(error);
+    throw new Error("error en conexión");
+  }
 };
